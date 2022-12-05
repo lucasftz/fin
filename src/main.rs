3b0c086ceacbb035b1e447cb12f1e9137e67d3b4
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-12-05 02:08:57 -0600

    Show files as a tree

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,66 @@
+mod stateful_tree;
 mod utils;
+
+use crate::stateful_tree::StatefulTree;
 use crate::utils::{restore, setup};
+use crossterm::event;
+use std::{env, fs, io};
+use tui_tree_widget::{Tree, TreeItem};
 
-use std::{env, fs, io, path, thread, time};
+fn get_tree_items(dir: &std::path::PathBuf) -> Vec<TreeItem<'static>> {
+    let mut tree_items: Vec<TreeItem> = Vec::new();
+    let mut files: Vec<TreeItem> = Vec::new();
 
-use tui::widgets::{List, ListItem};
+    for path in fs::read_dir(dir)
+        .unwrap()
+        .map(|entry| entry.unwrap().path())
+    {
+        if path.is_file() {
+            files.push(TreeItem::new_leaf(
+                path.file_name()
+                    .unwrap()
+                    .clone()
+                    .to_str()
+                    .unwrap()
+                    .to_owned(),
+            ));
+        } else {
+            tree_items.push(TreeItem::new(
+                path.file_name()
+                    .unwrap()
+                    .clone()
+                    .to_str()
+                    .unwrap()
+                    .to_owned(),
+                get_tree_items(&path),
+            ));
+        }
+    }
+
+    tree_items.append(&mut files);
+
+    return tree_items;
+}
 
 fn main() -> Result<(), io::Error> {
     let mut terminal = setup().unwrap();
 
-    let paths: Vec<path::PathBuf> = fs::read_dir(env::current_dir().unwrap())
-        .unwrap()
-        .map(|entry| entry.unwrap().path())
-        .collect();
+    let mut tree = StatefulTree::with_items(get_tree_items(&env::current_dir().unwrap()));
 
-    let (mut dirs, mut files) = (Vec::new(), Vec::new());
+    loop {
+        terminal.draw(|frame| {
+            let size = frame.size();
+            let items = Tree::new(tree.items.clone());
+            frame.render_stateful_widget(items, size, &mut tree.state);
+        })?;
 
-    for path in paths.iter() {
-        let item = ListItem::new(path.to_str().unwrap());
-
-        if path.is_file() {
-            files.push(item);
-        } else {
-            dirs.push(item);
-        }
+        if let event::Event::Key(key) = event::read()? {
+            match key.code {
+                event::KeyCode::Char('q') => break,
+                _ => {}
+            }
+        };
     }
-
-    dirs.append(&mut files);
-
-    terminal.draw(|frame| {
-        let size = frame.size();
-        let items = List::new(dirs);
-        frame.render_widget(items, size);
-    })?;
-
-    thread::sleep(time::Duration::from_millis(2000));
 
     restore(terminal)
 }
