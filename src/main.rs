31668c6e6136b0110470b3f2dd88dbe9a44f49e4
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-12-04 18:50:00 -0600

    Render files+folders in current directory on the screen

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 mod utils;
 use crate::utils::utils::{restore, setup};
 
-use std::{io, thread, time::Duration};
+use std::{env, fs, io, path, thread, time};
 
-use tui::widgets::{Block, Borders};
+use tui::widgets::{List, ListItem};
 
 fn main() -> Result<(), io::Error> {
     let mut terminal = setup().unwrap();
 
+    let paths: Vec<path::PathBuf> = fs::read_dir(env::current_dir().unwrap())
+        .unwrap()
+        .map(|entry| entry.unwrap().path())
+        .collect();
+
+    let items: Vec<ListItem> = paths
+        .iter()
+        .map(|path| ListItem::new(path.to_str().unwrap()))
+        .collect();
+
     terminal.draw(|frame| {
         let size = frame.size();
-        let block = Block::default().title("Block").borders(Borders::ALL);
-        frame.render_widget(block, size);
+        let list = List::new(items);
+        frame.render_widget(list, size);
     })?;
 
-    thread::sleep(Duration::from_millis(5000));
+    thread::sleep(time::Duration::from_millis(2000));
 
     restore(terminal)
 }
